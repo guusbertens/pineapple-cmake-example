0624d2dac3236b38737e1ca495ab8a81d60b1ebe
Guus Bertens <guus.bertens@gwdg.de>
2022-04-12 17:41:59 +0200

    make OpenSSL optional

--- Rng.cpp
+++ Rng.cpp
@@ -1,13 +1,17 @@
 #include "Rng.h"
 #include "SimpleRng.h"
+#ifdef ENABLE_MD5RNG
 #include "Md5Rng.h"
+#endif // ENABLE_MD5RNG
 
 std::unique_ptr<Rng> Rng::get(const std::string name)
 {
 	if (name == "simple")
 		return std::make_unique<SimpleRng>();
+#ifdef ENABLE_MD5RNG
 	if (name == "md5")
 		return std::make_unique<Md5Rng>();
+#endif // ENABLE_MD5RNG
 	return NULL;
 }
 
